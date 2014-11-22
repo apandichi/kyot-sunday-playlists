500c509ed5c503c4f22237bde76af8a668fa1baf
Alin Pandichi <alin.pandichi@gmail.com>
2014-11-22 15:44:01 +0200

    created parse kyot url function

--- program.js
+++ program.js
@@ -2,8 +2,12 @@ var http = require('http');
 
 var kyotUrl = 'http://quietmusic.com/kyot-sunday-playlists';
 
+var parseKyotUrl = function(data) {
+    console.log(data);
+}
+
 http.get(kyotUrl, function (response) {
   response.setEncoding('utf8')
-  response.on('data', console.log)
+  response.on('data', parseKyotUrl)
   response.on('error', console.error)
 });
